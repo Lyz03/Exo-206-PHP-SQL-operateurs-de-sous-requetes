83703fd312e5c24fb108201b6475f2f27c27e8b0
Lyz03 <lizoe@netcourrier.com>
2022-02-25 15:55:29 +0100

    exo 206

--- index.php
+++ index.php
@@ -10,3 +10,62 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+$db = new PDO('mysql:host=localhost;dbname=live;charset=utf8', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+// part 1
+$stmt = $db->prepare("SELECT * FROM user");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
+
+
+// part 2
+$stmt = $db->prepare("SELECT * FROM article");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
+
+// part 3
+$stmt = $db->prepare("
+        SELECT username FROM user
+            WHERE id = ANY (SELECT user_fk FROM article WHERE contenu LIKE '%poterie%')
+");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
+
+// part 4
+$stmt = $db->prepare("
+        SELECT username FROM user
+            WHERE id = ANY (SELECT user_fk FROM article HAVING count(user_fk) >= 2)
+");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
+
+// part 5
+$stmt = $db->prepare("
+        SELECT username FROM user
+            WHERE username LIKE 'jane%' AND id = ANY (SELECT user_fk FROM article)
+");
+
+if ($stmt->execute()) {
+    echo '<pre>';
+    print_r($stmt->fetchAll());
+    echo '</pre>';
+}
